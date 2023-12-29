aa5ff806465771b3594dc749ad639009362a9aef
JMaycol-Valencia <maycolvalencia50@gmail.com>
2023-12-29 00:42:47 -0400

    adventjs-reto-07-08

---
+++ agrupacion_string_08.js
@@ -0,0 +1,41 @@
+"use strict";
+//CAJA X 10 REGALOS = {a}
+//PALET X 5 CAJAS = [a] 
+//REGALO EN BOLSA = (bbbb) X 4
+//solucion by: "JmaycolV 🏴BO"
+function organizeGifts(gifts) {
+    //separamos los numeros con letra
+    const matches = gifts.match(/\d*\w/g);
+    //caso de vacio o cero coincidencias
+    if (!matches)
+        return gifts;
+    for (let match of matches) {
+        //extraemos el numro de la coincidenciaa
+        let count = Number(match.slice(0, -1));
+        //extraemos el caracter de laa coincidencia  
+        let gift = match.at(-1);
+        //inicializamos el valor que alojara nuestro resultado
+        let draft = "";
+        //dividimos el numerro entre 50
+        const palets = Math.floor(count / 50);
+        //actualizamos el valor 
+        count -= palets * 50;
+        //dividimos el numero entre 10
+        const boxes = Math.floor(count / 10);
+        //actualizamos el valor
+        count -= boxes * 10;
+        //imprimimos el resultado segun el caso que exista en el numero encontrado
+        if (palets)
+            draft += `[${gift}]`.repeat(palets);
+        if (boxes)
+            draft += `{${gift}}`.repeat(boxes);
+        if (count)
+            draft += `(${gift.repeat(count)})`;
+        //remplazamos el string original por el armado en draft
+        gifts = gifts.replace(match, draft);
+    }
+    return gifts;
+}
+organizeGifts(`71a11b`);
+//console.log(organizeGifts(`76a11b`))
+//[a]{a}{a}(a){b}(b) //rresultado esperrado
---
+++ imprimir_caracteres_07.ts
@@ -0,0 +1,57 @@
+//solucion by: "JmaycolV 🏴BO"
+function drawGift(size : number, symbol : string) {
+    if(size == 1) return '#\n'
+    
+    //inicializamos un string vacio
+    const filas:string[] = []
+    
+    //construimos las valores por defecto en cada caso que son el inicio, medio y final
+    //se usa interpolaccion de varriables y los metodos repeat para los elementos repetidos y padStart para los espacios
+    //inicio
+    filas[0] = '#'.repeat(size).padStart(size * 2 - 1)
+    //medio
+    filas[size - 1] = `${'#'.repeat(size)}${symbol.repeat(size - 2)}#`
+    //final
+    filas[size * 2 - 2] = '#'.repeat(size)
+    
+    //reccorremos por fila para los otros valores
+    for(let fila = 1; fila < size - 1; fila++){
+      //definimos el draft que sera la linea a pushear
+      let draft = `#${symbol.repeat(size-2)}#${symbol.repeat(fila - 1)}#`
+      
+      //agregamos el draft a las posiciones inicciales y  los espacios iniciales para las primeras filas que tiene espacio
+      filas[fila] = draft.padStart(size * 2 -1, ' ')
+      //agregamos el draft a las posiciones finales
+      filas[size * 2 - 2 - fila] = draft
+    }
+    
+    //retornamos el string unido en una respuesta separada por el salto de linea \n y el salto de linea extra al final
+    return filas.join('\n').concat('\n')
+}
+
+drawGift(5, '*')
+//resultado esperado
+/*
+    #####
+   #***##
+  #***#*#
+ #***#**#
+#####***#
+#***#**#
+#***#*#
+#***##
+#####
+*/
+
+//analisis de la solucion, verlo de otra manera inicialmente
+/*
+#####
+#***##
+#***#*#
+#***#**#
+#####***#
+#***#**#
+#***#*#
+#***##
+#####
+*/
